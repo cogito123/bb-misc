2500cbe67364484a3fa9714b356041803c19e2ed
cogito123 <59515242+cogito123@users.noreply.github.com>
2025-12-06 14:13:51 +0100

    biter-speed-scaling: add few improvements
    - Make speed scaling dynamic through 'storage.cbs.cycle', which
      represents interval in minutes. If interval has elapsed, speed stat is
      increased by another 100%.
    - Added better description.
    - Excluded bosses from the mechanics as they would be too difficult
      otherwise.

--- biter-speed-scaling.lua
+++ biter-speed-scaling.lua
@@ -5,6 +5,7 @@ if storage.cbs then
 end
 
 storage.cbs = {
+    cycle = 60,
     info = {},
 }
 
@@ -21,23 +22,26 @@ function create_description()
 
   local texts = {
         '-> Biters speed scaling by cogito <-',
-        'Biters gain movement and attack speed with match time',
+        'Regular biters gain movement and attack speed over the course of a match',
+        'Movement speed starts increasing once attack speed reaches 200%',
         'a',
-        'b'
+        'b',
+        'c'
   }
 
   for i, text in ipairs(texts) do
-    local color = i % 2 == 0 and { 255, 255, 0 } or { 255, 200, 0 }
-    storage.cbs['info'][i] = rendering.draw_text {
-      text = text,
-      surface = game.player.surface,
-      target = { entity = chest, offset = { 0, 1.25 * i } },
-      color = color,
-      scale = 1.00,
-      font = "heading-1",
-      alignment = "center",
-      scale_with_zoom = true
-    }
+        local color = { 255, 200, 0 }
+        color = i > 4 and { 221, 160, 221 } or color
+        storage.cbs['info'][i] = rendering.draw_text {
+          text = text,
+          surface = game.player.surface,
+          target = { entity = chest, offset = { 0, 1.25 * i } },
+          color = color,
+          scale = 1.00,
+          font = "heading-1",
+          alignment = "center",
+          scale_with_zoom = true
+        }
   end
 end
 
@@ -51,12 +55,16 @@ local adjust_unit_speed = [[function(event)
     return
   end
 
+  if string.find(entity.force.name, "_boss", 1, true) then
+    return
+  end
+
   local functions = require("maps.biter_battles_v2.functions")
   local ticks = functions.get_ticks_since_game_start()
   local minutes = ticks / 3600
 
   local base_spd = entity.prototype.speed
-  local factor = math.max(1, minutes / 60)
+  local factor = math.max(1, minutes / storage.cbs.cycle)
   local spd = base_spd * factor
 
   entity.speed = spd
@@ -78,23 +86,22 @@ local on_tick = [[function(event)
   local functions = require("maps.biter_battles_v2.functions")
   local ticks = functions.get_ticks_since_game_start()
   local minutes = ticks / 3600
-  local melee_spd = minutes / 60
-  local biological_spd = minutes / 60
+  local melee_spd = minutes / storage.cbs.cycle
+  local biological_spd = minutes / storage.cbs.cycle
   local forces = {
     "north_biters",
-    "north_biters_boss",
     "south_biters",
-    "south_biters_boss",
-    }
+  }
   for _, name in ipairs(forces) do
       local f = game.forces[name]
       f.set_gun_speed_modifier("melee", melee_spd)
       f.set_gun_speed_modifier("biological", biological_spd)
   end
 
-  local mov_spd = math.max(1, minutes / 60)
-  storage.cbs['info'][3].text = string.format('Current movement speed at %.2f%%', mov_spd * 100)
-  storage.cbs['info'][4].text = string.format('Current attack speed at %.2f%%', (melee_spd + 1) * 100)
+  local mov_spd = math.max(1, minutes / storage.cbs.cycle)
+  storage.cbs['info'][4].text = string.format('Speed increases by 100%% every %d minutes', storage.cbs.cycle)
+  storage.cbs['info'][5].text = string.format('Current movement speed: %.2f%%', mov_spd * 100)
+  storage.cbs['info'][6].text = string.format('Current attack speed: %.2f%%', (melee_spd + 1) * 100)
 end]]
 
 event.add_removable_function(
